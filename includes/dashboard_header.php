983391bbad9808d0be20474cf0e05184713690ab
arinze96 <edmund100arinze@gmail.com>
2022-08-19 09:13:52 -0700

    first commit

--- includes/dashboard_header.php
+++ includes/dashboard_header.php
@@ -478,10 +478,10 @@ s0.parentNode.insertBefore(s1,s0);
         </div>
         <div>
           <a class="navbar-brand brand-logo" href="<?= $uri->site ?>">
-            <img src="../images/logo-v1.png" alt="Centralcreditsunion Logo" class="logo" />
+            <img src="../images/logo-v.png" alt="Centralcreditsunion Logo" class="logo" />
           </a>
           <a class="navbar-brand brand-logo-mini" href="<?= $uri->site ?>">
-            <img src="../images/logo-v1.png" alt="Centralcreditsunion Logo" class="logo" style="width:200px !important; height:100px !important;" />
+            <img src="../images/logo-v.png" alt="Centralcreditsunion Logo" class="logo" style="width:200px !important; height:100px !important;" />
           </a>
         </div>
       </div>
